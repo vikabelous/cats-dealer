8ad4e435d787e90e70a5afe75f40584860593ccd
Viktoriia Bilous <vikabelous@gmail.com>
2020-04-15 23:44:12 +0300

    update controller and views

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,5 +1,13 @@
 class CatsController < ApplicationController
-  def index
-    @cats = []
+  def search
+    return @cats = [] unless search_params?
+
+    @cats = CatsDealers.new.find_offers(params[:type], params[:location])
+  end
+
+  private
+
+  def search_params?
+    params[:type].present? && params[:location].present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'cats#index'
+  root 'cats#search'
+
+  get :search,  to: 'cats#search'
 end
